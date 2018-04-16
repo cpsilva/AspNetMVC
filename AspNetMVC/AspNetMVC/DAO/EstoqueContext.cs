160ff840b122f224ff660de1f37c441016e95be4
cpsilva <caiopirees@gmail.com>
2018-04-16 12:36:07 -0300

    Gerado banco de dados e classes DAO da aplicação

---
+++ AspNetMVC/AspNetMVC/DAO/EstoqueContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetMVC.Models;
+using System.Data.Entity;
+
+namespace AspNetMVC.DAO
+{
+    public class EstoqueContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<CategoriaDoProduto> Categorias { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ AspNetMVC/AspNetMVC/DAO/ProdutosDAO.cs
@@ -0,0 +1,46 @@
+﻿using AspNetMVC.DAO;
+using AspNetMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CaelumEstoque.DAO
+{
+    public class ProdutosDAO
+    {
+        public void Adiciona(Produto produto)
+        {
+            using (var context = new EstoqueContext())
+            {
+                context.Produtos.Add(produto);
+                context.SaveChanges();
+            }
+        }
+
+        public IList<Produto> Lista()
+        {
+            using (var contexto = new EstoqueContext())
+            {
+                return contexto.Produtos.Include("Categoria").ToList();
+            }
+        }
+
+        public Produto BuscaPorId(int id)
+        {
+            using (var contexto = new EstoqueContext())
+            {
+                return contexto.Produtos.Include("Categoria")
+                    .Where(p => p.Id == id)
+                    .FirstOrDefault();
+            }
+        }
+
+        public void Atualiza(Produto produto)
+        {
+            using (var contexto = new EstoqueContext())
+            {
+                contexto.Entry(produto).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
