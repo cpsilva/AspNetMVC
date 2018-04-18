d35395385dca38f466603112dcac53968cf7fdc7
cpsilva <caiopirees@gmail.com>
2018-04-18 09:45:44 -0300

    Craido autenticação e tela de login

---
+++ AspNetMVC/AspNetMVC/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using AspNetMVC.DAO;
+using AspNetMVC.Models;
+using System.Web.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Authentication(string login, string senha)
+        {
+            var dao = new UsuariosDAO();
+
+            var usuario = dao.Busca(login, senha);
+
+            if (usuario != null)
+            {
+                Session["usuarioAutenticado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
